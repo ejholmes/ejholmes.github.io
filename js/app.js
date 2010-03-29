5da6ef32f8205affa9e1aa58a0983cb9fad030b6
U-ejholmes-laptop\ejholmes <ejholmes@ejholmes-laptop.(none)>
2010-03-29 03:02:16 -0700

    0.1
    Signed-off-by:  <>

--- js/app.js
+++ js/app.js
@@ -1,12 +1,4 @@
 $(document).ready(function(){
-	/*$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?id=47865405@N02&lang=en-us&format=json&nojsoncallback=1", function(data){
-		alert(data);
-		$.each(data.items, function(i, item){
-			$("<img/>").attr("src", item.media.m).appendTo("#flickr");
-			if( i == 3)
-				return false;
-		});
-	});*/
 	$('#flickr').jflickrfeed({
 		limit: 14,
 		qstrings: {
@@ -14,7 +6,15 @@ $(document).ready(function(){
 		},
 		itemTemplate: 
 		'<li>' +
-			'<a href="{{image_b}}"><img src="{{image_s}}" alt="{{title}}" /></a>' +
+			'<a href="{{image_b}}" class="photos"><img src="{{image_s}}" alt="{{title}}" /></a>' +
 		'</li>'
+	}, function(){
+		bind();
 	});
+	
+	bind();
 });
+
+function bind(){
+	$("#flickr a").fancybox();
+}
