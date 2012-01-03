5314ce955b5befa08a13b8d4b127bb06b5dcfa79
Eric J. Holmes <eric@ejholmes.net>
2012-01-03 09:57:39 -0800

    Remove old flickr stuff

--- js/application.js
+++ js/application.js
@@ -1,23 +1,3 @@
-$(document).ready(function(){
-	$('#flickr').jflickrfeed({
-		limit: 14,
-		qstrings: {
-			nsid: '47865405@N02',
-			set: '72157623732861328'
-		},
-		feedapi: 'photoset.gne',
-		itemTemplate: 
-		'<li>' +
-			'<a href="{{image_b}}" class="photos"><img src="{{image_s}}" alt="{{title}}" /></a>' +
-		'</li>'
-	}, function(){
-		bind();
-	});
-	
-	bind();
+$(function(){
+    // Do something
 });
-
-function bind(){
-	$("#flickr a").fancybox();
-	$("#content a[href*='flickr.com']").fancybox();
-}
